267faca32a3cb3dce8c4a7f28f53817b2388bae9
OReoN03 <gorkov.artem03@mail.ru>
2026-05-17 17:49:02 +0300

    Add email uniqueness check

--- src/main/java/com/example/online_cinema/service/RegistrationService.java
+++ src/main/java/com/example/online_cinema/service/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.example.online_cinema.service;
 
+import com.example.online_cinema.exception.UserAlreadyExistsException;
 import com.example.online_cinema.repository.UserRepository;
 import com.example.online_cinema.security.RegistrationForm;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,8 +18,11 @@ public class RegistrationService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    // TODO check uniqueness of email
     public void processRegistration(RegistrationForm form) {
+        String email = form.getEmail();
+        if (userRepository.existsByEmail(email)) {
+            throw new UserAlreadyExistsException(email);
+        }
         userRepository.save(form.toUser(passwordEncoder));
     }
 }
